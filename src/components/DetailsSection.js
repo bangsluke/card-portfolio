462b6a6de900f096b04102ce62a44d1e7c250fda
Luke Bangs <bangsluke@gmail.com>
2022-06-07 22:42:30 +0100

    Initial creation of Portoflio card as part of React tutorial

---
+++ src/components/DetailsSection.js
@@ -0,0 +1,16 @@
+import * as React from "react";
+
+function DetailsSection() {
+	return (
+		<div className='details-section'>
+			<h3>About</h3>
+			<p>
+				An enthusiastic new developer, just starting out on my development path, keen to develop my coding interests.
+			</p>
+			<h3>Interests</h3>
+			<p>I'm interested in developing a few personal projects such as a stats webite for my lcoal football club.</p>
+		</div>
+	);
+}
+
+export default DetailsSection;
