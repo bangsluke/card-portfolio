3c4c026a0b1bd0b21a45a7b5bc20c3fc8f7a170e
Luke Bangs <bangsluke@gmail.com>
2022-06-08 16:56:24 +0100

    Updates to Portfolio card and introduction of MUI components

--- src/data/ButtonDataArray.js
+++ src/data/ButtonDataArray.js
@@ -1,19 +1,19 @@
-import emailIcon from "../images/email.png";
-import linkedInIcon from "../images/linkedin.png";
+// import emailIcon from "../images/email.png";
+// import linkedInIcon from "../images/linkedin.png";
 
-export const ButtonDataArray = [
-	{
-		id: 1,
-		url: "mailto:bangsluke@gmail.com",
-		buttonText: "Email",
-		icon: emailIcon,
-	},
-	{
-		id: 2,
-		url: "https://www.linkedin.com/in/bangsluke/",
-		buttonText: "LinkedIn",
-		icon: linkedInIcon,
-	},
-];
+// export const ButtonDataArray = [
+// 	{
+// 		id: 1,
+// 		url: "mailto:bangsluke@gmail.com",
+// 		buttonText: "Email",
+// 		icon: emailIcon,
+// 	},
+// 	{
+// 		id: 2,
+// 		url: "https://www.linkedin.com/in/bangsluke/",
+// 		buttonText: "LinkedIn",
+// 		icon: linkedInIcon,
+// 	},
+// ];
 
-export default ButtonDataArray;
+// export default ButtonDataArray;
