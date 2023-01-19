22277cac1ebc9ad7b18d1a7fd3fc2a233a6e52e5
Luke Bangs <61457009+bangsluke@users.noreply.github.com>
2023-01-19 15:52:25 +0000

    Added skills and categorised the bubbles

--- src/components/FloatingBubbles.js
+++ src/components/FloatingBubbles.js
@@ -3,6 +3,8 @@ import HighchartsReact from "highcharts-react-official";
 import HighchartsMore from "highcharts/highcharts-more";
 import HighchartsAccessibility from "highcharts/modules/accessibility";
 import HighchartsExporting from "highcharts/modules/exporting";
+import { skillData } from "../data/skillData";
+
 HighchartsExporting(Highcharts);
 HighchartsAccessibility(Highcharts);
 HighchartsMore(Highcharts);
@@ -29,88 +31,7 @@ const getOptions = (type) => ({
 			},
 		},
 	},
-	series: [
-		{
-			name: "A",
-			data: [
-				{
-					name: "A.1",
-					value: 2,
-				},
-				{
-					name: "A.2",
-					value: 10,
-				},
-				{
-					name: "A.3",
-					value: 9,
-				},
-				{
-					name: "A.4",
-					value: 12,
-				},
-			],
-		},
-		{
-			name: "B",
-			data: [
-				{
-					name: "B.1",
-					value: 2,
-				},
-				{
-					name: "B.2",
-					value: 1,
-				},
-				{
-					name: "B.3",
-					value: 8,
-				},
-				{
-					name: "B.4",
-					value: 7,
-				},
-				{
-					name: "B.5",
-					value: 5,
-				},
-			],
-		},
-		{
-			name: "C",
-			data: [
-				{
-					name: "C.1",
-					value: 11,
-				},
-				{
-					name: "C.2",
-					value: 7,
-				},
-				{
-					name: "C.3",
-					value: 2,
-				},
-			],
-		},
-		{
-			name: "D",
-			data: [
-				{
-					name: "D.1",
-					value: 7,
-				},
-				{
-					name: "D.2",
-					value: 4,
-				},
-				{
-					name: "D.3",
-					value: 9,
-				},
-			],
-		},
-	],
+	series: skillData,
 	credits: {
 		enabled: false,
 	},
