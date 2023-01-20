635c13f93a551b0e7c73207327ba70f01ee57bfb
Luke Bangs <bangsluke@gmail.com>
2023-01-20 16:28:04 +0000

    Tidied up card

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
-import "./App.css";
-// import CardComponent from "./components/CardComponent.js";
 import "@fontsource/roboto/300.css";
 import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
+import "./App.css";
+import CardComponent from "./components/CardComponent.js";
 import FloatingBubbles from "./components/FloatingBubbles";
 
 function App() {
 	return (
 		<div className='App'>
-			{/* <CardComponent /> */}
+			<CardComponent />
 			<FloatingBubbles />
 		</div>
 	);
